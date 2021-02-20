502ecac67363dd2d5359beb9e87f738dc6d59bb1
Stephen Keogh <stevokeogh@hotmail.com>
2021-02-20 15:07:34 +0000

    added product count to searchs and catergories

--- products/views.py
+++ products/views.py
@@ -5,9 +5,8 @@ from .models import Product, Category
 
 # Create your views here.
 
-
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all products """
 
     products = Product.objects.all()
     query = None
@@ -16,21 +15,20 @@ def all_products(request):
     direction = None
 
     if request.GET:
-
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
-        products = products.order_by(sortkey)
+            products = products.order_by(sortkey)
             
-
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -39,8 +37,9 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "Please Try Serach Again")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
@@ -51,7 +50,6 @@ def all_products(request):
         'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
-
     }
 
     return render(request, 'products/products.html', context)
