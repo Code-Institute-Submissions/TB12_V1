f811cc4e95ab11a138c4d3b972cf5617ad522950
Stephen Keogh <stevokeogh@hotmail.com>
2021-02-25 21:06:43 +0000

    added stripe payment complete

--- checkout/models.py
+++ checkout/models.py
@@ -22,15 +22,20 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
-        Generate order number using UUID
+        Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
-
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
+        """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
@@ -41,7 +46,7 @@ class Order(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Save method to set the order number
+        Override the original save method to set the order number
         if it hasn't been set already.
         """
         if not self.order_number:
@@ -61,7 +66,7 @@ class OrderLineItem(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Save method to set the lineitem total
+        Override the original save method to set the lineitem total
         and update the order total.
         """
         self.lineitem_total = self.product.price * self.quantity
