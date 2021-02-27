8290cb8832d9182926eb2b6a469caf9a91549593
Stephen Keogh <stevokeogh@hotmail.com>
2021-02-27 15:58:15 +0000

    fixed order history issue

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,3 +1,10 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
@@ -21,7 +28,7 @@ var style = {
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-// validation errors on the card element
+// Handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
@@ -37,7 +44,7 @@ card.addEventListener('change', function (event) {
     }
 });
 
-//  form submit
+// Handle form submit
 var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
@@ -48,6 +55,7 @@ form.addEventListener('submit', function(ev) {
     $('#loading-overlay').fadeToggle(100);
 
     var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    // From using {% csrf_token %} in the form
     var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
     var postData = {
         'csrfmiddlewaretoken': csrfToken,
@@ -105,6 +113,7 @@ form.addEventListener('submit', function(ev) {
             }
         });
     }).fail(function () {
+        // just reload the page, the error will be in django messages
         location.reload();
     })
 });
