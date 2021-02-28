7fe66d8a79f1bd36af4f77f0b052eafa8d034d78
Stephen Keogh <stevokeogh@hotmail.com>
2021-02-28 14:17:38 +0000

    deploy part 1-1

--- tb12_v1/settings.py
+++ tb12_v1/settings.py
@@ -122,16 +122,13 @@ WSGI_APPLICATION = 'tb12_v1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
+ DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+    }
+ }
 
-DATABASES = {
-    'default': dj_database_url.parse('postgres://krmfznyhdspfsk:07ae183dcfb12f2377f156d244510119d5c9b3078b62b6c289e5b68f59eb1941@ec2-54-72-155-238.eu-west-1.compute.amazonaws.com:5432/d3rpev8n4udq4j')
-}
 
 
 # Password validation
